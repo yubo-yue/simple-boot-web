a69af9b012d7ce01a17a873ffd5abb635e5e5123
Yu.Yue <yueyu@xiaomi.com>
2015-04-15 11:16:20 +0800

    initial

---
+++ src/main/java/org/simple/SimpleWebWithBootApplication.java
@@ -0,0 +1,12 @@
+package org.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebWithBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleWebWithBootApplication.class, args);
+    }
+}
---
+++ src/test/java/org/simple/SimpleWebWithBootApplicationTests.java
@@ -0,0 +1,18 @@
+package org.simple;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SimpleWebWithBootApplication.class)
+@WebAppConfiguration
+public class SimpleWebWithBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
